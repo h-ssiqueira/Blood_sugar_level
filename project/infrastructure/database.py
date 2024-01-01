1c43ff1340a1db89a811eccbda32759422a3cd58
Henrique <hss@outlook.com.br>
2024-01-01 15:12:02 -0300

    2023 update

--- project/infrastructure/database.py
+++ project/infrastructure/database.py
@@ -3,12 +3,13 @@ from glob import glob
 import logging
 from mysql.connector.connection import MySQLConnection
 from os.path import basename,dirname,realpath
+from os import getenv
 from pandas import read_csv
 
 def stablishConnection():
-    conn = MySQLConnection(user = 'root',
-                           password = 'root',
-                           host = '127.0.0.1',
+    conn = MySQLConnection(user = getenv('MYSQL_USERNAME', 'root'),
+                           password = getenv('MYSQL_PASSWORD', 'root'),
+                           host = getenv('MYSQL_HOST', '127.0.0.1'),
                            database = 'glicemia')
     logging.info("Connection to database stablished.")
     return conn, conn.cursor()
@@ -30,15 +31,14 @@ def mountDatabase(cursor, conn, dataDir):
 
 
 def importData(cursor,conn,dataDir):
-    files = [f for f in glob(''.join([dataDir,'*.csv']))]
-    logging.info(f"{len(files)} csv files found. Importing data.")
+    files = [f for f in glob(''.join([dataDir,'/formatted/*.csv']))]
+    logging.info(f"{len(files)} CSV files found. Importing data...")
     for file in files:
         year = basename(file).replace(".csv",'')
         cursor.execute("SELECT COUNT(*) FROM blood_sugar_level WHERE YEAR(date) = " + year)
         if cursor.fetchone()[0] == 0:
-            logging.info(f"Inserting data from year: {year}.")
+            logging.info(f"Inserting data from {year}.")
             file = read_csv(file, sep=',')
-            print(file.head())
             for _, row in file.iterrows():
                 parsed_date = datetime.strptime(row['Date'], "%a. %d/%b.")
                 cursor.execute("""
@@ -47,9 +47,9 @@ def importData(cursor,conn,dataDir):
                     VALUES (STR_TO_DATE(%s, '%Y-%m-%d'), %s, %s, %s, %s, %s, %s, %s, %s);
                 """, (f"{year}-{parsed_date.month}-{parsed_date.day}", row['Before breakfast'], row['2h after breakfast'], row['Before lunch'], row['2h after lunch'], row['Before dinner'], row['2h after dinner'], row['Extra'], row['Comment']))
             conn.commit()
-            logging.info(f"Data from year {year} inserted successfully.")
+            logging.info(f"Data from {year} inserted successfully.")
         else:
-            logging.info(f"Data from year {year} already imported.")
+            logging.info(f"Data from {year} already imported.")
 
 def updateViews(cursor,conn,dataDir):
     logging.info("Updating views.")
@@ -61,7 +61,8 @@ def updateViews(cursor,conn,dataDir):
     logging.info("Views updated successfully.")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='[%(threadName)s:%(filename)s:%(funcName)s:%(lineno)d]|[%(asctime)s]|[%(levelname)s]: %(message)s')
+    logging.getLogger('mysql.connector').setLevel(logging.WARNING)
     dataDir = dirname(realpath(__file__)).replace("project/infrastructure","data/")
     conn, cursor = stablishConnection()
     mountDatabase(cursor,conn,dataDir)
--- project/util/languageUtil.py
+++ project/util/languageUtil.py
@@ -1,40 +1,61 @@
 from glob import glob
-from os.path import dirname,realpath
+from re import compile
+from os.path import basename,dirname,realpath
 from pandas import read_csv
+import logging
 
-def mapPortugueseToEnglish():
-    dataDir = dirname(realpath(__file__)).replace("project/util","data/")
-    columnTypes = {'Before breakfast': 'Int64',
-                '2h after breakfast': 'Int64',
-                'Before lunch': 'Int64',
-                '2h after lunch': 'Int64',
-                'Before dinner': 'Int64',
-                '2h after dinner': 'Int64',
-                'Extra': 'Int64'}
-    dayMapping = {'seg': 'Mon',
-                'ter': 'Tue',
-                'qua': 'Wed',
-                'qui': 'Thu',
-                'sex': 'Fri',
-                'sáb': 'Sat',
-                'dom': 'Sun'}
-    monthMapping = {'jan': 'Jan',
-                    'fev': 'Feb',
-                    'mar': 'Mar',
-                    'abr': 'Apr',
-                    'mai': 'May',
-                    'jun': 'Jun',
-                    'jul': 'Jul',
-                    'ago': 'Aug',
-                    'set': 'Sep',
-                    'out': 'Oct',
-                    'nov': 'Nov',
-                    'dez': 'Dec'}
-    for file in [f for f in glob(''.join([dataDir,'*.csv']))]:
-        data = read_csv(file, sep=',', dtype=columnTypes)
-        if data['Date'].str.contains('sáb').any():
+columnTypes = {'Before breakfast': 'Int64',
+               '2h after breakfast': 'Int64',
+               'Before lunch': 'Int64',
+               '2h after lunch': 'Int64',
+               'Before dinner': 'Int64',
+               '2h after dinner': 'Int64',
+               'Extra': 'Int64'}
+dayMapping = {'seg': 'Mon',
+              'ter': 'Tue',
+              'qua': 'Wed',
+              'qui': 'Thu',
+              'sex': 'Fri',
+              'sáb': 'Sat',
+              'dom': 'Sun'}
+monthMapping = {'jan': 'Jan',
+                'fev': 'Feb',
+                'mar': 'Mar',
+                'abr': 'Apr',
+                'mai': 'May',
+                'jun': 'Jun',
+                'jul': 'Jul',
+                'ago': 'Aug',
+                'set': 'Sep',
+                'out': 'Oct',
+                'nov': 'Nov',
+                'dez': 'Dec'}
+
+def formatCSV():
+    dataDir = dirname(realpath(__file__)).replace("project/util","data/formatted/")
+    files = [f for f in glob(''.join([dataDir,'*.csv']))]
+    pattern = compile(r'^(([^,]*,){8}[^,]*)')
+    logging.info(f"{len(files)} files found. Formatting data...")
+    for file in files:
+        year = basename(file).replace('.csv','')
+        with open(file, 'r') as fileContent:
+            lines = fileContent.readlines()
+        if "manhã" in lines[0]:
+            logging.info(f"Formatting {year} file.")
+            lines = [pattern.match(line).group(1) + '\n' if pattern.match(line) else line for line in lines]
+            lines[:2] = ["Date,Before breakfast,2h after breakfast,Before lunch,2h after lunch,Before dinner,2h after dinner,Extra,Comment\n"]
+            with open(file, 'w') as fileContent:
+                fileContent.writelines(lines)
+            data = read_csv(file, sep=',', dtype=columnTypes)
             for portugueseDay, englishDay in dayMapping.items():
                 data['Date'] = data['Date'].str.replace(portugueseDay, englishDay)
             for portugueseMonth, englishMonth in monthMapping.items():
                 data['Date'] = data['Date'].str.replace(portugueseMonth, englishMonth)
             data.to_csv(file, index=False)
+            logging.info(f"{year} formatted successfully.")
+        else:
+            logging.info(f"{year} already formatted.")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='[%(threadName)s:%(filename)s:%(funcName)s:%(lineno)d]|[%(asctime)s]|[%(levelname)s]: %(message)s')
+    formatCSV()
